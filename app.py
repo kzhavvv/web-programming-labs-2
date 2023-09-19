486eacfcad72c47cecb7633a678b4add746364e6
Жвавий Карина <karinazvavij@gmail.com>
2023-09-20 01:42:50 +0700

    Первая страница лабораторной

--- app.py
+++ app.py
@@ -2,4 +2,21 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html lang="ru">
+    <head>
+    </head>
+    <body>
+        <header>
+        НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+        &copy; Жвавий Карина, ФБИ-14, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
