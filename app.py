661bd449631f172cdb38c12553b2cb6dbbcb1778
Жвавий Карина <karinazvavij@gmail.com>
2023-10-04 02:09:21 +0700

    Книги

--- app.py
+++ app.py
@@ -208,4 +208,16 @@ def example():
         {'name':'мандарины', 'price': 95},
         {'name':'манго', 'price': 321}
         ]
-    return render_template('example.html',name=name,group=group,curs=curs,laba=laba,fruits=fruits)
+    books =[
+        {'bk': 'Оно', 'autor': 'Стивен Кинг', 'genre': 'роман','pages': 1138 },
+        {'bk': 'Три товарища', 'autor': 'Эрих Мария Ремарк', 'genre': 'роман','pages': 480 },
+        {'bk': 'Портрет Дориана Грея', 'autor': 'Оскар Уайльд', 'genre': 'роман','pages': 400 },
+        {'bk': 'Не ной', 'autor': 'Джен Синсеро', 'genre': 'Саморазвитие','pages': 292 },
+        {'bk': '451° по Фаренгейту', 'autor': 'Рей Бредбери', 'genre': 'антиутопия','pages': 256 },
+        {'bk': 'Гарри Поттер', 'autor': 'Джоан Роулинг', 'genre': 'фэнтези','pages': 248 },
+        {'bk': 'После', 'autor': 'Анна Тод', 'genre': 'Современная проза','pages': 448 },
+        {'bk': 'Маленький принц', 'autor': 'Антуан де Сент-Экзюпери', 'genre': 'повесть','pages': 110 },
+        {'bk': 'Великий Гэтсби', 'autor': 'Фрэнсис Скотт Фицджеральд', 'genre': 'роман','pages':256  },
+        {'bk': 'Дневник Анны Франк', 'autor': 'Анна Франк', 'genre': 'мемуар','pages': 280 }
+    ]
+    return render_template('example.html',name=name,group=group,curs=curs,laba=laba,fruits=fruits,books=books)
