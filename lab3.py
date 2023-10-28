a896e4cef8b89acdc73e102cdc637f2d47fbb721
Жвавий Карина <karinazvavij@gmail.com>
2023-10-28 23:05:16 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, redirect, url_for,render_template, request
 lab3 = Blueprint('lab3',__name__)
 
+
 @lab3.route('/lab3/')
 def lab():
     return render_template('lab3.html')
+
 
 @lab3.route('/lab3/form1')
 def form1():
@@ -17,3 +19,33 @@ def form1():
         errorss['age'] = 'Заполните поле!'
     sex = request.args.get('sex')
     return render_template('form1.html', user=user, age=age, sex=sex, errors=errors, errorss=errorss)
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    if drink == 'cofee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+    if request.args.get('cinnamon') == 'on':
+        price += 0
+    return render_template('pay.html', price=price)
+
+
+@lab3.route('/lab3/success')
+def success():
+    return render_template('success.html')
