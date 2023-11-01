fea1f476bfe8825e414cd6ebbe32778aac800075
Жвавий Карина <karinazvavij@gmail.com>
2023-11-01 12:36:56 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -4,3 +4,25 @@ lab4 = Blueprint('lab4',__name__)
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
+
+
+@lab4.route('/lab4/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+    error = None 
+    if not username and not password:
+        error = 'Нет логина и пароля'
+    elif not username:
+        error = 'Нет логина'
+    elif not password:
+        error = 'Нет пароля'
+    elif username == 'kzhavvv' and password == '2023':
+        return render_template("entrance.html", username=username)
+    else:
+        error = 'Неверный логин или пароль!'
+
+    return render_template("login.html", error=error, username=username, password=password)
