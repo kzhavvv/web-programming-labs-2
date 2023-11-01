8235c1eb73a3b79f8a440089042d50b8ce4a4980
Жвавий Карина <karinazvavij@gmail.com>
2023-11-01 12:53:54 +0700

    Заказ зерна

--- lab4.py
+++ lab4.py
@@ -26,3 +26,66 @@ def login():
         error = 'Неверный логин или пароль!'
 
     return render_template("login.html", error=error, username=username, password=password)
+
+
+@lab4.route('/lab4/fridge', methods=['GET', 'POST'])
+def fridge():
+    if request.method == 'GET':
+        return render_template("fridge.html")
+    
+    temperature = request.form.get('temperature')
+    if  not temperature:
+        result = 'ошибка: не задана температура!'
+    elif int(temperature) < -12:
+        result = 'не удалось установить температуру — слишком низкое значение'
+    elif int(temperature) > -1:
+        result = 'не удалось установить температуру — слишком высокое значение'
+    elif int(temperature) > -12 and int(temperature) < -9:
+        result = f"Установлена температура: {temperature}°C ***"
+    elif int(temperature) >= -8 and int(temperature) <= -5:
+        result = f"Установлена температура: {temperature}°C **"
+    elif int(temperature) >= -4 and int(temperature) <= -1:
+        result = f"Установлена температура: {temperature}°C *"
+
+    return render_template("fridge.html", result=result)
+
+
+@lab4.route('/lab4/zerno', methods=['GET', 'POST'])
+def zerno():
+    if request.method == 'GET':
+        return render_template("zerno.html")
+    grain = request.form.get('grain')
+    weight = float(request.form.get('weight'))
+    price = 0
+    skidka = None
+
+    if grain == 'Ячмень':
+        price = 12000
+    elif grain == 'Овёс':
+        price = 8500
+    elif grain == 'Пшеница':
+        price = 8700
+    elif grain == 'Рожь':
+        price = 14000
+    else:
+        price = 0
+    
+    summ = price * weight
+    if weight >= 50 and weight <= 500:
+        summ = (price * weight) * 0.9
+        skidka_itog = (price * weight) * 0.1
+        skidka = f'Скидка 10% за объём более 50 тон: = {skidka_itog}'
+    elif weight < 50:   
+        skidka = 'Скидка 10% за объём не присвоена'
+    else:
+        skidka = None
+
+ 
+    if weight > 500:
+        errors = 'Такого объема нет в наличии'
+        return render_template("zerno.html", errors=errors)
+    elif weight < 0:
+        errors = 'Не верно указан объём закупки'
+        return render_template("zerno.html", errors=errors)
+    
+    return render_template("zerno.html", grain=grain, weight=weight, summ=summ, skidka=skidka)
