a449cee6a873fb9e3e1a3c72759033a3e6e03823
Жвавий Карина <karinazvavij@gmail.com>
2023-11-15 18:10:37 +0700

    дошла до заметок

--- lab5.py
+++ lab5.py
@@ -1,5 +1,6 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask import Blueprint, render_template, request, session, redirect, url_for
+from flask import session 
 import psycopg2
 
 
@@ -82,16 +83,118 @@ def anon():
     return render_template('anon.html', username=VisibleUser)
 
 
-@lab5.route('/lab5/loginn')
+@lab5.route("/lab5/newtitle", methods = ["GET", "POST"])
+def createArticle():
+    errors = []
+
+    userID = session.get('id')
+    user_name = session.get('user_name')
+    if userID is not None:
+        if request.method =="GET":
+            return render_template("newtitle.html", user_name=user_name)
+        
+        if request.method == "POST":
+            article_text = request.form.get("article_text")
+            title = request.form.get("article_title")
+
+            if len(article_text)==0:
+                errors.append("Заполните текст")
+                return render_template("new_article", errors = errors, user_name=user_name)
+            
+            conn = dbConnect()
+            cur = conn.cursor()
+
+            cur.execute("INSERT INTO articles (user_id, title, article_text) VALUES (%s, %s, %s) RETURNING id;", (userID, title, article_text))
+            
+            
+            new_articl_id = cur.fetchone()[0]
+            conn.commit()
+
+            dbClose(cur, conn)
+
+            return redirect(f"/lab5/articles/{new_articl_id}")
+
+    return redirect("/lab5/login5")
+
+
+@lab5.route('/lab5/loginn', methods=["GET", "POST"])
 def loginn():
-    return render_template('loginn.html')
+    errors = []
+
+    if request.method == "GET":
+        return render_template("loginn.html", errors=errors)
+    
+    user_name = request.form.get("username")
+    password = request.form.get("password")
+
+    if not (user_name or password):
+        errors.append("Пожалуйста заполните все поля")
+        return render_template("loginn.html", errors=errors)
+    
+    conn = dbConnect()
+    cur = conn.cursor()
+
+    cur.execute("SELECT id, password FROM users WHERE username = %s;", (user_name,))
+
+    result = cur.fetchone()
+
+    if result is None:
+        errors.append("Неправильный логин или пароль")
+        dbClose(cur, conn)
+        return render_template("loginn.html", errors=errors)
+    
+    userID, hashPassword = result
+
+    if check_password_hash(hashPassword, password):
+        session['id'] = userID
+        session['user_name'] = user_name
+        dbClose(cur, conn)
+        return redirect("/lab5/")
+    
+    else:
+        errors.append("Неправильный логин или пароль")
+        return render_template("loginn.html", errors=errors)
 
 
-@lab5.route('/lab5/sozdanie')
-def sozdanie():
-    return render_template('sozdanie.html')
+@lab5.route("/lab5/articles/<int:article_id>")
+def getArticle(article_id):
+    userID = session.get('id')
+
+    if userID is not None:
+        conn = dbConnect()
+        cur = conn.cursor()
+
+        cur.execute("SELECT title, article_text FROM articles WHERE id = %s AND user_id = %s", (article_id, userID))
+
+        articleBody = cur.fetchone()
+
+        dbClose(cur, conn)
+
+        if articleBody is None:
+            return "Not found!"
+        
+        text = articleBody[1].splitlines()
+
+    return render_template("articleN.html", article_text=text, article_title=articleBody[0], user_name=session.get("user_name"))
 
 
-@lab5.route('/lab5/prosmotr')
-def prosmotr():
-    return render_template('prosmotr.html')
+
+@lab5.route('/lab5/articles')
+def list_articles():
+    userID = session.get('id')
+    user_name = session.get("user_name")
+    
+    if userID is not None:
+        conn = dbConnect()
+        cur = conn.cursor()
+        
+        cur.execute("SELECT id, title FROM articles WHERE user_id = %s;", (userID,))
+        articles_data = cur.fetchall()
+        
+        articles = [{'id': row[0], 'title': row[1]} for row in articles_data]
+
+        dbClose(cur, conn)
+
+        return render_template('articles.html', articles=articles, user_name=user_name)
+
+    return redirect("/lab5/login5")
