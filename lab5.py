b74c220a03a712093cc4e0d852c234eb654185d0
Жвавий Карина <karinazvavij@gmail.com>
2023-11-22 17:56:48 +0700

    Лабораторная работа 6

--- lab5.py
+++ lab5.py
@@ -7,6 +7,7 @@ import psycopg2
 lab5 = Blueprint('lab5', __name__)
 
 def dbConnect():
+    #подключение к бд
     conn = psycopg2.connect(
         host = "127.0.0.1",
         database = "knowledge_base",
@@ -15,19 +16,25 @@ def dbConnect():
     return conn;
 
 def dbClose(cursor, connection):
+    #закрываем курсор и соединение 
+    #курсор - память, которая хранит  набор select 
+    #select возвращает данные
     cursor.close()
     connection.close()
+
+
 @lab5.route('/lab5')
 def main():
     conn = dbConnect()
     cur = conn.cursor()
-
+    #запрос, который psycopg2 должен выполнить 
     cur.execute("SELECT * FROM users;")
-
+    #fetchall - получить все строки, которые получились в результате
+    #выполнения SQL-запроса в execute
     result = cur.fetchall()
 
     print(result)
-
+    #закрытие соединения с БД
     dbClose(cur,conn)
 
     return "go to console"
@@ -42,35 +49,41 @@ def lab():
 @lab5.route('/lab5/reg', methods=["GET", "POST"])
 def reg():
     errors = []
-
+    #если это метод GET, то вернуть шаблон и завершить выполнение
     if request.method == "GET":
         return render_template('reg.html', errors=errors)
-     
+    #если мы здесь, то это метод POST    
     user_name = request.form.get("user_name")
     password = request.form.get("password")
 
-
+    #Проверка username u password на пустоту
+    #если из них кто-то пустой, то добавим ошибку и рендерим шаблон
     if not (user_name or password):
         errors.append("Пожалуйста заполните все поля")
         print(errors)
         return render_template('reg.html', errors=errors)
-    
+    #получаем пароль от пользователя и хешируем его
     hashPassword = generate_password_hash(password)
-
+    #если мы тут, то значит username u password заполнены 
+    #подключаемся к БД
     conn = dbConnect()
     cur = conn.cursor()
-
+    #проверяем наличие клиента в бд
+    #у нас не может быть два пользователя с одинаковыми логинами
     cur.execute("SELECT username FROM users WHERE username = %s;", (user_name,))
-
+    #fetchone получает одну строку
+    #только один пользователь может быть с таким именем в бд
     if cur.fetchone() is not None:
         errors.append("Пользователь с данным именем уже существует")
 
         conn.close()
         cur.close()
         return render_template('reg.html', errors=errors)
-    
+    #если мы попали сюда, то значит в cur.fetchone нет ни одной строки
+    #пользователя с таким логином не существует
+    #сохраняем пароль в виде хэша Бд   
     cur.execute("INSERT INTO users (username, password) VALUES (%s, %s);", (user_name, hashPassword))
-    
+    #делаем commit - т.е. фиксируем изменени
     conn.commit()
     conn.close()
     cur.close()
@@ -144,8 +157,11 @@ def loginn():
         return render_template("loginn.html", errors=errors)
     
     userID, hashPassword = result
-
+    #с помощью check_password_hash сравниваем пароль и хэш из бд
     if check_password_hash(hashPassword, password):
+        #пароль правильный 
+
+        #сохраняем id u username в сессию 
         session['id'] = userID
         session['user_name'] = user_name
         dbClose(cur, conn)
@@ -158,9 +174,11 @@ def loginn():
 
 @lab5.route("/lab5/articles/<int:article_id>")
 def getArticle(article_id):
-    userID = session.get('id')
 
+    userID = session.get('id')
+    #проверка авторизирован ли пользователь
     if userID is not None:
+
         conn = dbConnect()
         cur = conn.cursor()
 
@@ -172,7 +190,7 @@ def getArticle(article_id):
 
         if articleBody is None:
             return "Not found!"
-        
+        #разбиваем строку на массив по "Enter",чтобы с помощью цикла for разбить статью на параграфы 
         text = articleBody[1].splitlines()
 
     return render_template("articleN.html", article_text=text, article_title=articleBody[0], user_name=session.get("user_name"))
@@ -181,14 +199,16 @@ def getArticle(article_id):
 
 @lab5.route('/lab5/articles')
 def list_articles():
+    #проверяем авторизирован ли пользователь
     userID = session.get('id')
     user_name = session.get("user_name")
-    
+    #если пользователь авторизирован
     if userID is not None:
         conn = dbConnect()
         cur = conn.cursor()
         
         cur.execute("SELECT id, title FROM articles WHERE user_id = %s;", (userID,))
+    #получаем id от вновь созданной записи, получаем статьи след.образом /lab5/articles/id_articles
         articles_data = cur.fetchall()
         
         articles = [{'id': row[0], 'title': row[1]} for row in articles_data]
@@ -196,8 +216,8 @@ def list_articles():
         dbClose(cur, conn)
 
         return render_template('articles.html', articles=articles, user_name=user_name)
-
-    return redirect("/lab5/login5")
+    #пользователь не авторизирован
+    return redirect("/lab5/loginn")
 
 @lab5.route('/lab5/logout', methods=["GET"])
 def logout():
